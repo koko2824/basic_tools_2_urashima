202baa573bb6da6aeb309156162d4a2e86a49a07
koko2824 <prg.koko2824@gmail.com>
2022-08-03 03:32:09 +0900

    fix animate.css version

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,23 @@
 const moveSection = (from, to) => {
   const current = document.getElementById(`section${from}`);
   const next = document.getElementById(`section${to}`);
-  current.classList.add("fadeOutLeft");
+  current.classList.add("animate__fadeOutLeft");
   current.addEventListener("animationend", () => {
     current.style.display = "none";
     next.style.display = "block";
-    next.classList.add("fadeInRight");
+    next.classList.add("animate__fadeIn");
   });
 };
 
 const specialMoveSection = () => {
   const current = document.getElementById("section4");
   const next = document.getElementById("section5");
-  current.classList.add("fadeOut");
+  const smoke = document.getElementById("smoke");
+  current.classList.add("animate__fadeOut");
   current.addEventListener("animationend", () => {
     current.style.display = "none";
-    next.classList.add("fadeIn");
+    next.classList.add("animate__fadeIn");
     next.style.display = "block";
+    smoke.classList.add("animate__animated");
   });
 };
