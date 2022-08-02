7adabfbe6db4358015fa18b913725044de7074c7
koko2824 <prg.koko2824@gmail.com>
2022-08-03 03:16:56 +0900

    fix text and animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,21 @@
-
 const moveSection = (from, to) => {
   const current = document.getElementById(`section${from}`);
   const next = document.getElementById(`section${to}`);
-
-  current.classList.add("fadeOutRight")
-  current.addEventListener('animationend', () => {
-  current.style.display = "none";
-  next.style.display = "block";
-  next.classList.add("fadeInLeft")
-  })
+  current.classList.add("fadeOutLeft");
+  current.addEventListener("animationend", () => {
+    current.style.display = "none";
+    next.style.display = "block";
+    next.classList.add("fadeInRight");
+  });
 };
 
 const specialMoveSection = () => {
-  const current = document.getElementById('section3');
-  const next = document.getElementById('section4');
-  current.style.display = "none";
-  next.classList.add('fadeIn')
-  next.style.display = "block";
-}
-
-
+  const current = document.getElementById("section4");
+  const next = document.getElementById("section5");
+  current.classList.add("fadeOut");
+  current.addEventListener("animationend", () => {
+    current.style.display = "none";
+    next.classList.add("fadeIn");
+    next.style.display = "block";
+  });
+};
